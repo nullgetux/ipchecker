38f95ffdc13ebb19d4ee814909baf950354c64ed
nullgtx <chupunk4@gmail.com>
2025-06-18 14:53:50 +0700

    update-api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function (Request $request) {
+    return response()->json(['ip' => $request->ip()]);
+});
+
+Route::get('/ip', function (Request $request) {
+    return response()->json(['ip' => $request->ip()]);
+});
